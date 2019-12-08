afa4914f9fa9079b2ef44e00d068e0a7c88fc4f5
baybay24 <mystery_castle1124@hotmail.com>
2019-12-08 11:26:37 +0800

    自动登录淘宝，选择商品

--- src/main/java/test/TestNg.java
+++ src/main/java/test/TestNg.java
@@ -1,8 +1,14 @@
 package test;
 
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class TestNg {
+    @BeforeClass
+    public void testBefore(){
+        System.out.println("Before test NG");
+    }
+
     @Test
     public void test(){
         System.out.println("test NG");
